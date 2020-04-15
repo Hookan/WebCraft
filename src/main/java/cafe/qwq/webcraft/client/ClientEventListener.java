3d81f4e50e24494d414b4339f03a544ff66c70fd
hookan <916811837@qq.com>
2020-04-15 23:39:45 +0800

    修复在Windows下崩溃的bug

--- src/main/java/cafe/qwq/webcraft/client/ClientEventListener.java
+++ src/main/java/cafe/qwq/webcraft/client/ClientEventListener.java
@@ -3,9 +3,12 @@ package cafe.qwq.webcraft.client;
 import cafe.qwq.webcraft.Config;
 import cafe.qwq.webcraft.WebCraft;
 import cafe.qwq.webcraft.util.FileUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -25,32 +28,32 @@ import java.util.zip.ZipFile;
 @Mod.EventBusSubscriber(modid = WebCraft.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventListener
 {
-    private static Consumer<TickEvent.RenderTickEvent> consumer;
+    private static Consumer<GuiOpenEvent> consumer;
     private static String MAVEN_URL = "https://maven.qwq.cafe/";
     private static String libPath;
-
+    
     static
     {
-        if(WebCraft.RUNTIME_OS== WebCraft.OS.WINDOWS)
+        if (WebCraft.RUNTIME_OS == WebCraft.OS.WINDOWS)
             MAVEN_URL = "https://ci.qwq.cafe/maven/";
-
+        
         if (!WebCraft.VERSION.equals("NONE") && !WebCraft.VERSION.equals("NULL"))
         {
             libPath = "mods/webcraft/natives-" + WebCraft.VERSION + "/";
             if (!checkNatives()) downloadNatives();
         }
         else libPath = System.getProperty("cafe.qwq.webcraft.nativesPath");
-
+        
         if (WebCraft.RUNTIME_OS == WebCraft.OS.WINDOWS)
         {
             loadLibrary("UltralightCore");
             loadLibrary("WebCore");
             loadLibrary("Ultralight");
         }
-
+        
         loadLibrary("webcraft_core");
     }
-
+    
     private static String getNativePath(String name)
     {
         String name1;
@@ -68,27 +71,27 @@ public class ClientEventListener
         }
         return libPath + name1;
     }
-
+    
     private static boolean checkNatives()
     {
         File f = new File(libPath);
         if (!f.exists() || !f.isDirectory()) return false;
-
+        
         File f1 = new File(getNativePath("UltralightCore"));
         if (!f1.exists() || !f1.isFile()) return false;
-
+        
         f1 = new File(getNativePath("WebCore"));
         if (!f1.exists() || !f1.isFile()) return false;
-
+        
         f1 = new File(getNativePath("Ultralight"));
         if (!f1.exists() || !f1.isFile()) return false;
-
+        
         f1 = new File(getNativePath("webcraft_core"));
         if (!f1.exists() || !f1.isFile()) return false;
-
+        
         return true;
     }
-
+    
     private static void downloadNatives()
     {
         String fileName = "webcraft-" + WebCraft.VERSION + "-natives";
@@ -104,7 +107,7 @@ public class ClientEventListener
                 fileName += "-mac.jar";//虽然并没有MAC版本
                 break;
         }
-
+        
         String urlstr = MAVEN_URL + "cafe/qwq/webcraft/" + WebCraft.VERSION + "/" + fileName;
         try
         {
@@ -117,10 +120,10 @@ public class ClientEventListener
             else
             {
                 System.setProperty("java.awt.headless", "false");
-
+                
                 JFrame frame = new JFrame();
                 JPanel panel = new JPanel();
-
+                
                 panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
                 frame.setContentPane(panel);
                 frame.setTitle("WebCraft");
@@ -134,7 +137,7 @@ public class ClientEventListener
                 frame.setResizable(false);
                 frame.setUndecorated(true);
                 frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
+                
                 panel.setBackground(Color.WHITE);
                 JLabel label = new JLabel("Downloading " + fileName + "...");
                 label.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -143,13 +146,13 @@ public class ClientEventListener
                 bar.setMaximum(1000);
                 panel.add(bar);
                 frame.setVisible(true);
-
+                
                 final String finalFileName = fileName;
                 FileUtils.downloadFile(url, outputFile, (size, downloadSize, speed) -> {
                     label.setText(String.format("Downloading %s(%.2fMB/%.2fMB %.2fMB/s)...", finalFileName, downloadSize / 1024f, size / 1024f, speed / 1024f));
                     bar.setValue((int) (downloadSize * 1000 / size));
                 });
-
+                
                 frame.dispose();
             }
             ZipFile zip = new ZipFile(outputFile);
@@ -183,16 +186,23 @@ public class ClientEventListener
             e.printStackTrace();
         }
     }
-
+    
     private static void loadLibrary(String name)
     {
         File f = new File(getNativePath(name));
         System.load(f.getAbsolutePath());
     }
-
+    
     @SubscribeEvent
     public static void onClientSetup(final FMLClientSetupEvent event)
     {
+        consumer = ClientEventListener::onGuiOpen;
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGHEST, consumer);
+    }
+    
+    public static void onGuiOpen(final GuiOpenEvent event)
+    {
+        MinecraftForge.EVENT_BUS.unregister(consumer);
         WebCraft.LOGGER.info("Start loading Ultralight...");
         UltralightWindow.init();
         UltralightWindow.getInstance().makeCurrent();
@@ -201,8 +211,8 @@ public class ClientEventListener
         nativeInit(GLFW.Functions.GetProcAddress, GLFW.Functions.GetTime, configPointer);
         UltralightWindow.getInstance().unmakeCurrent();
     }
-
+    
     private native static long setNativeConfig(String fontFamilyStandard, String fontFamilyFixed, String fontFamilySerif, String fontFamilySansSerif, String userAgent);
-
+    
     private native static void nativeInit(long pointer1, long pointer2, long pointer3);
 }
